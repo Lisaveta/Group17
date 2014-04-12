ca7e1e9bc8ce22a587b98752369eb131bcafefab
Lisaveta <myttk.ru>
2014-04-12 17:19:06 +0700

    ДЗ18: Разработать тесты для создания контактов

---
+++ addressbook-native-tests/src/com/example/fw/ContactHelper.java
@@ -0,0 +1,45 @@
+package com.example.fw;
+
+
+public class ContactHelper extends HelperBase{
+
+	public ContactHelper(ApplicationManager applicationManager) {
+		super(applicationManager);
+	}
+
+	public void createContact(Contact contact) {
+		initContactCreation();
+		fillContactForm(contact);
+		confirmContactCreation();
+	}
+	private void initContactCreation() {
+		manager.getAutoItHelper()
+		.winWaitAndActivate("AddressBook Portable", "", 5000)
+		.click("Add").winWaitAndActivate("Add Contact", "", 5000);
+	}
+	private void fillContactForm(Contact contact) {
+		manager.getAutoItHelper()
+		.send("TDBEdit12", contact.firstname)
+		.send("TDBEdit11", contact.lastname);
+	}
+	private void confirmContactCreation() {
+		manager.getAutoItHelper()
+		.click("Save")
+		.winWaitAndActivate("AddressBook Portable", "", 5000);
+	}
+	public Contact getFirstContact() {
+		manager.getAutoItHelper()
+		.click("TListView1")
+		.send("{DOWN}{SPACE}")
+		.click("Edit")
+		.winWaitAndActivate("Update Contact", "", 5000);
+		Contact contact = new Contact()
+		.setFirstName(manager.getAutoItHelper().getText("TDBEdit12"))
+		.setLastName(manager.getAutoItHelper().getText("TDBEdit11"));
+	manager.getAutoItHelper()
+		.click("Cancel")	
+		.winWaitAndActivate("Update Contact", "", 5000);
+		return contact;
+	}
+
+}
